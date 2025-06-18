a07e2b094b30f47d9559e23b3c7dec70c72b677d
ManCat18 <cmanon452003@gmail.com>
2025-06-18 15:35:45 +0200

    MAJ police

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import { Mail, Phone } from 'lucide-react';
 const Footer = () => {
     return (
         <footer className="bg-amber-900 text-white py-8 mt-16">
-            <div className="max-w-6xl mx-auto px-6 text-center text-base">
+            <div className="max-w-6xl mx-auto px-6 text-center ">
                 <p className="mb-4">Manon Cattaneo - Portfolio 2025</p>
-                <div className="flex flex-col md:flex-row justify-center items-center sm:gap-2 md:gap-6text-amber-200 text-center">
+                <div className="flex flex-col md:flex-row justify-center items-center sm:gap-2 md:gap-6 text-amber-200 text-center text-base">
                     <div className="flex items-center gap-2">
                         <Mail className="w-4 h-4" />
                         <span>cattaneo.manon1@gmail.com</span>
