77e48c9216a80901d9ba17ffeb1cd60eb0aec43a
ManCat18 <cmanon452003@gmail.com>
2025-06-18 15:32:50 +0200

    Modification du header en responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ const Header = ({ age }) => {
         <div className="w-20 h-20 sm:w-10 sm:h-10 md:w-18 md:h-18 mx-auto mt-2 mb-4 bg-gradient-to-br from-amber-500 to-orange-600 rounded-full flex items-center justify-center text-white text-xl sm:text-xl md:text-2xl font-bold shadow-xl">
           MC
         </div>
-        <h1 className="text-xl font-bold text-amber-900 mb-4">Manon CATTANEO</h1>
-        <p className="text-base text-amber-800 mb-8">Étudiante L3 Informatique • En recherche d'alternance</p>
+        <h1 className="text-2xl font-bold text-amber-900 mb-4">Manon CATTANEO</h1>
+        <p className="text-1g text-amber-800 mb-8">Étudiante L3 Informatique • En recherche d'alternance</p>
 
         <div className="flex flex-col md:flex-row justify-center items-center gap-4 md:gap-8 mb-8">
           <div className="bg-white/80 rounded-lg p-6 shadow-lg w-full md:w-auto text-center">
@@ -30,19 +30,19 @@ const Header = ({ age }) => {
 
 
         <div className="flex flex-col md:flex-row flex-wrap justify-center items-start gap-4 md:gap-6 text-amber-800 text-md text-center md:text-left pb-6">
-          <div className="text-sm flex items-center gap-2">
+          <div className="text-1g flex items-center gap-2">
             <Mail className="w-5 h-5" />
             <span>cattaneo.manon1@gmail.com</span>
           </div>
-          <div className="text-sm flex items-center gap-2">
+          <div className="text-1g flex items-center gap-2">
             <Phone className="w-5 h-5" />
             <span>+33 6 21 52 67 65</span>
           </div>
-          <div className="text-sm flex items-center gap-2">
+          <div className="text-1g flex items-center gap-2">
             <MapPin className="w-5 h-5" />
             <span>BLOIS 41000</span>
           </div>
-          <div className="text-sm flex items-center gap-2">
+          <div className="text-1g flex items-center gap-2">
             <Car className="w-5 h-5" />
             <span>Permis B • avec voiture</span>
           </div>
