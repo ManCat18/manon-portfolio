2e8508473b27ae6c784c577cb62da63149e2f192
ManCat18 <cmanon452003@gmail.com>
2025-06-20 13:31:38 +0200

    Modification du responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,15 +14,15 @@ const Header = ({ age }) => {
         <p className="text-1g text-amber-800 mb-8">Étudiante L3 Informatique • En recherche d'alternance</p>
 
         <div className="flex md:flex-row justify-center items-center gap-4 md:gap-8 mb-8">
-          <div className="bg-white/80 rounded-lg p-6 shadow-lg w-full md:w-auto text-center">
+          <div className="bg-white/80 rounded-lg md:p-6 sm:p-auto shadow-lg w-full md:w-auto text-center">
             <div className="text-3xl font-bold text-amber-800">3</div>
             <div className="text-sm text-amber-600">ans d'études</div>
           </div>
-          <div className="bg-white/80 rounded-lg p-6 shadow-lg w-full md:w-auto text-center">
+          <div className="bg-white/80 rounded-lg md:p-6 sm:p-auto shadow-lg w-full md:w-auto text-center">
             <div className="text-3xl font-bold text-amber-800">{age}</div>
             <div className="text-sm text-amber-600">ans</div>
           </div>
-          <div className="bg-white/80 rounded-lg p-6 shadow-lg w-full md:w-auto text-center">
+          <div className="bg-white/80 rounded-lg md:p-6 sm:p-auto shadow-lg w-full md:w-auto text-center">
             <div className="text-3xl font-bold text-amber-800">8+</div>
             <div className="text-sm text-amber-600">technologies</div>
           </div>
